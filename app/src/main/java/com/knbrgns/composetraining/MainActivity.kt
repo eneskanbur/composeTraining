90e45f0ae9a5367ff231b5747138e3c3863d603c
Enes Kanbur <eneskanbur02@hotmail.com>
2025-11-04 19:51:37 +0300

    First composable

--- app/src/main/java/com/knbrgns/composetraining/MainActivity.kt
+++ app/src/main/java/com/knbrgns/composetraining/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.knbrgns.composetraining.ui.theme.ComposeTrainingTheme
 
@@ -19,11 +21,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeTrainingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface() {
+
                 }
             }
         }
@@ -31,17 +30,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyBusinessCard(name: String, title: String, modifier: Modifier = Modifier) {
+    Text(text = name, color = Color.Blue)
+    Text(text = title, color = Color.Red)
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeTrainingTheme {
-        Greeting("Android")
-    }
+fun MyBusinessCardPreview() {
+    MyBusinessCard(name = "name", title = "title")
 }
